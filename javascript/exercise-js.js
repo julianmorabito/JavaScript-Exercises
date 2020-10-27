4427c1ec8cc6a2516bb4e00febcdf78d03b15587
Julian Morabito <julianmorabito@gmail.com>
2020-10-27 15:35:26 -0300

    PS-04: theglitchfront.html added, with css and images.

---
+++ javascript/exercise-js.js
@@ -0,0 +1,7 @@
+//--------Variables and operators---------
+
+// part a) create two numerical variables and use the operator sum for save the value of the sum of both numbers on a 3rd variable.
+var num1 = 405;
+var num2 = 234;
+var num3 = num1 + num2;
+alert (num3);
