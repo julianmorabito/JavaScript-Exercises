8571891067615d00fcd74e0ab762d1b1ac28c607
Julian Morabito <julianmorabito@gmail.com>
2020-10-27 15:40:56 -0300

    PS-04: exercise 1)b) added

--- javascript/exercise-js.js
+++ javascript/exercise-js.js
@@ -5,3 +5,9 @@ var num1 = 405;
 var num2 = 234;
 var num3 = num1 + num2;
 alert (num3);
+
+// part b) create two string type variables and concatenate them saving the result on a 3rd variable.
+var string1 = "Radium ";
+var string2 = "Rocket.";
+var string3 = string1 + string2;
+alert (string3);
